5bf33027cea53fe3fdb9c701133061c5293d4e1e
tefx <zhaomeng.zhu@gmail.com>
2023-02-24 14:32:24 +0800

    add DBMixin

--- {{ cookiecutter.__project_folder }}/{{ cookiecutter.__project_slug }}/db/models.py
+++ {{ cookiecutter.__project_folder }}/{{ cookiecutter.__project_slug }}/db/models.py
@@ -1,2 +1,2 @@
-from dcwiz_app_utils.db import DBBase  # DO NOT REMOVE THIS LINE, EVEN IF IT IS NOT USED
+from dcwiz_app_utils import DBBase, DBMixin  # DO NOT REMOVE THIS LINE, EVEN IF IT IS NOT USED
 from sqlalchemy import Column
