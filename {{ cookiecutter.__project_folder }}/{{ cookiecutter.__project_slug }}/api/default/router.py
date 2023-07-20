d6f1c1011cf63899a22cf02771f4ac659e69f8f6
tefx <zhaomeng.zhu@gmail.com>
2023-07-20 12:18:05 +0800

    add code to get bearer

--- {{ cookiecutter.__project_folder }}/{{ cookiecutter.__project_slug }}/api/default/router.py
+++ {{ cookiecutter.__project_folder }}/{{ cookiecutter.__project_slug }}/api/default/router.py
@@ -1,9 +1,13 @@
 from dcwiz_app_utils import get_config, APIRouter
+from fastapi import Depends
+from fastapi.security import HTTPBearer
 
 from .schemas import HealthStatus
 
+
 router = APIRouter()
 config = get_config()
+auth_scheme = HTTPBearer()
 
 
 @router.on_event("startup")
@@ -16,6 +20,12 @@ async def startup():
 @router.get("/health")
 async def health() -> HealthStatus:
     return HealthStatus(status="OK")
+
+
+@router.get("/")
+async def first_endpoint(bearer=Depends(auth_scheme)):
+    bearer = bearer.credentials
+    raise NotImplementedError
 
 
 # Define your endpoints here
